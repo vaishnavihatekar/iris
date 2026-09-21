cf2890cd2c7e2eec3a6cd434f03e04f94c841ccc
vaishnavihatekar <vaishnavihatekar1001@gmail.com>
2026-09-21 12:11:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title('Iris Flower Prediction App')
 st.header('Enter the measurements of the Iris flower:')
 
 #input title
-sepal_length=st.number_input('Sepal Length (cm)',min_values=0.0,max_values=10.0,values=5.0,step=0.1)
+sepal_length = st.number_input('Sepal Length (cm)', min_value=0.0, max_value=10.0, value=5.0, step=0.1)
 sepal_width=st.number_input('Sepal width (cm)',min_values=0.0,max_values=10.0,values=3.0,step=0.1)
 petal_length=st.number_input('Petal Length (cm)',min_values=0.0,max_values=8.0,values=4.0,step=0.1)
 petal_width=st.number_input('Petal Width (cm)',min_values=0.0,max_values=5.0,values=1.0,step=0.1)
