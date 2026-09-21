d832b9babaa174bc59d4bfeb60ab5a330cbe0efc
vaishnavihatekar <vaishnavihatekar1001@gmail.com>
2026-09-21 12:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.header('Enter the measurements of the Iris flower:')
 sepal_lenght=st.number_input('Sepal Length (cm)',min_values=0.0,max_values=10.0,values=5.0,step=0.1)
 sepal_width=st.number_input('Sepal width (cm)',min_values=0.0,max_values=10.0,values=3.0,step=0.1)
 petal_lenght=st.number_input('Petal Length (cm)',min_values=0.0,max_values=8.0,values=4.0,step=0.1)
-petal_width=st.number_input('Petal Width (cm)',min_values=0.0,max_values=10.0,values=1.0,step=0.1)
+petal_width=st.number_input('Petal Width (cm)',min_values=0.0,max_values=5.0,values=1.0,step=0.1)
 
 #prediction
 if st.button('predict'):
